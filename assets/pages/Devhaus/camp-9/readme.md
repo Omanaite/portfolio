cb264edc3b70008670304d68343d72716d67b9a9
Omanaite <pchandia@hotmail.com>
2023-03-08 21:59:51 +0100

    first commit
    header responsive done

---
+++ assets/pages/Devhaus/camp-9/readme.md
@@ -0,0 +1,7 @@
+&nbsp;
+
+# Devhaus Leipzig
+
+![Devhaus Leipzig, A learning community for programmers](/assets/img/devhaus-logo.svg) 
+##### Jan 26 - Jul 05
+Content created to put into practice the knowledge acquired at Devhaus during the bootcamp following the learning by ***doing approach***.
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import './style.css'
+import '../assets/css/source/output/input.css'
+
+const burgerTogglerRef = document.getElementById("burger-toggler")!;
+
+
+function handleClick() {
+  const ref = document.getElementById("sm-menu-ref")!;
+  ref.classList.toggle("hidden");
+}
+
+burgerTogglerRef!.onclick = handleClick;
