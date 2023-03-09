7fe1b1d5dc4add4ecfc0847d2d13064ccf0de324
Omanaite <pchandia@hotmail.com>
2023-03-09 06:53:49 +0100

    body ready

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,10 @@ import '../assets/css/source/output/input.css'
 
 const burgerTogglerRef = document.getElementById("burger-toggler")!;
 
-
 function handleClick() {
   const ref = document.getElementById("sm-menu-ref")!;
   ref.classList.toggle("hidden");
+  ref.classList.toggle("come-front-menu");
 }
 
 burgerTogglerRef!.onclick = handleClick;
