d6df881ee94f8cc15a463244ae6602db3738c10b
Omanaite <pchandia@hotmail.com>
2023-03-09 21:46:37 +0100

    last skeleton changes

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import '../src/style.css'
 import '../assets/css/source/output/input.css'
 
 const burgerTogglerRef = document.getElementById("burger-toggler")!;
+const burgerTogglerRef1 = document.getElementById("sm-menu-ref-1")!;
+const burgerTogglerRef2 = document.getElementById("sm-menu-ref-2")!;
+const burgerTogglerRef3 = document.getElementById("sm-menu-ref-3")!;
+const burgerTogglerRef4 = document.getElementById("sm-menu-ref-4")!;
 
 function handleClick() {
   const ref = document.getElementById("sm-menu-ref")!;
@@ -10,3 +14,10 @@ function handleClick() {
 }
 
 burgerTogglerRef!.onclick = handleClick;
+burgerTogglerRef1!.onclick = handleClick;
+burgerTogglerRef2!.onclick = handleClick;
+burgerTogglerRef3!.onclick = handleClick;
+burgerTogglerRef4!.onclick = handleClick;
+
+
+
